e5f8ceb45383d8fa865c4482b226439eecb4eaf7
ayushman chauhan <chauhanayushman505@gmail.com>
2026-08-06 17:36:45 +0530

    web changes post Form

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,4 @@
+import PostForm from "./PostForm"
 
 export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080/v1"
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <h1 className="text-3xl  text-center "  >GOPHER SOCIAL </h1>
+      <PostForm />
     </>
   )
 }
